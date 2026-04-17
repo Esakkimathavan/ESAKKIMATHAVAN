5fd29f8d4b3501aca553f3c57048a8719b1ec963
Esakkimathavan <esakkimathavan17@gmail.com>
2026-04-17 22:41:25 +0530

    Initial commit

---
+++ webdesign.html/domevent.js
@@ -0,0 +1,7 @@
+h1=document.getElementById("files");
+h1=addEventListener("mouseover",function()){
+h1.inerHTML="DOCUMENT OBJECT MODEL"
+h1.Style.color="red";
+h1.style.backgroundcolor="black";
+};
+ 
